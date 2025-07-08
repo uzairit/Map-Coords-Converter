9c46f3b65c153160f91bca353b67942f55039935
uzairit <uzair18it@gmail.com>
2025-07-09 04:30:09 +0500

    Add Function

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const convertBtn = document.getElementById('convertBtn')
 const map = document.getElementById('map')
 const MyLocationBtn = document.getElementById('MyLocationBtn')
 
+function showMap(latitude, longitude) {
+    const iframe = document.createElement('iframe')
+    iframe.src = `https://www.google.com/maps/?q=${latitude},${longitude}&output=embed`;
+    iframe.allowFullscreen = true;
+    iframe.loading = "lazy";
+    iframe.referrerPolicy = "no-referrer-when-downgrade";
+    iframe.sandbox = "allow-scripts allow-same-origin allow-popups";
+    map.innerHTML = "";
+    map.appendChild(iframe);
+    map.style.display = 'block';
+}
+
 convertBtn.addEventListener('click', () => {
     if (lat.value && lon.value) {
-        const iframe = document.createElement('iframe')
-        iframe.src = `https://www.google.com/maps/?q=${latitude},${longitude}&output=embed`;
-        iframe.allowFullscreen = true;
-        iframe.loading = "lazy";
-        iframe.referrerPolicy = "no-referrer-when-downgrade";
-        iframe.sandbox = "allow-scripts allow-same-origin allow-popups";
-        map.innerHTML = "";
-        map.appendChild(iframe);
-        map.style.display = 'block';
+        showMap(lat.value, lon.value)
     }
     else {
         alert('Please fill Both Section')
@@ -23,19 +27,9 @@ convertBtn.addEventListener('click', () => {
 })
 MyLocationBtn.addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition(position => {
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
-        lat.value = latitude;
-        lon.value = longitude;
-        const iframe = document.createElement('iframe')
-        iframe.src = `https://www.google.com/maps/?q=${latitude},${longitude}&output=embed`;
-        iframe.allowFullscreen = true;
-        iframe.loading = "lazy";
-        iframe.referrerPolicy = "no-referrer-when-downgrade";
-        iframe.sandbox = "allow-scripts allow-same-origin allow-popups";
-        map.innerHTML = "";
-        map.appendChild(iframe);
-        map.style.display = 'block';
+        lat.value = position.coords.latitude;
+        lon.value = position.coords.longitude;
+        showMap(lat.value, lon.value)
     }, () => {
         alert("Location access denied or unavailable.");
     });
